95515011f5c7154beede6ed631674a1f61d44414
alisoltanirad <soltaniradali@gmail.com>
2023-08-25 16:34:24 -0400

    Add feature selection

--- src/logic/data.py
+++ src/logic/data.py
@@ -10,14 +10,15 @@ import pandas as pd
 from config import BQ_DATASET, BQ_PROJECT, BQ_TABLE
 
 
-def load_training_data_from_bigquery():
+def load_training_data_from_bigquery() -> pd.DataFrame:
     """Load training data from BigQuery
 
     This function makes a query to BigQuery to load customer churn training data
-    and returns the data in the shape of a Pandas dataframe
+    and returns the data in the shape of a Pandas dataframe.
 
     Returns:
         pd.DataFrame: Customer churn training data
+
     """
     # SQL query to retrieve training data from BigQuery
     query: str = f"""
@@ -31,8 +32,55 @@ def load_training_data_from_bigquery():
     return dataset
 
 
+def split_x_y(dataset: pd.DataFrame) -> tuple[pd.DataFrame, pd.Series]:
+    """Split features and labels
+
+    Args:
+        dataset (pd.DataFrame): Training data
+
+    Returns:
+        pd.DataFrame: X (features)
+        pd.Series: y (labels)
+
+    """
+    return dataset[dataset.columns.difference(["Exited"])], dataset["Exited"]
+
+
+def select_features(X: pd.DataFrame) -> pd.DataFrame:
+    """Feature selection
+
+    This function selects features useful to train the model.
+
+    Args:
+        X (pd.DataFrame): Feature set containing all features
+
+    Returns:
+        pd.DataFrame: Feature set containing selected features
+
+    """
+    return X[[
+        "CreditScore",
+        "Geography",
+        "Gender",
+        "Age",
+        "Tenure",
+        "Balance",
+        "NumOfProducts",
+        "HasCrCard",
+        "IsActiveMember",
+        "EstimatedSalary",
+        "Complain",
+        "Satisfaction_Score",
+        "Card_Type",
+        "Point_Earned"
+    ]]
+
+
 if __name__ == "__main__":
     dataset: pd.DataFrame = load_training_data_from_bigquery()
-    print(dataset.shape)
-    print(dataset.columns)
-    print(dataset.head())
+    X, y = split_x_y(dataset)
+    X = select_features(X)
+    print(X.shape)
+    print(X.columns)
+    print(X.head())
+    print(y.head())
