a6586df5d8cb74fb738ee6d9b34d82c3eed8dc50
alisoltanirad <soltaniradali@gmail.com>
2023-08-26 18:34:10 -0400

    Add a sample data to test prediction

--- src/logic/prediction.py
+++ src/logic/prediction.py
@@ -49,3 +49,21 @@ if __name__ == "__main__":
     classifier, preprocessor = load_from_gcs(GCS_FOLDER)
     print(classifier)
     print(preprocessor)
+    sample = pd.DataFrame({
+        "CreditScore": [821, 630],
+        "Geography": ["France", "France"],
+        "Gender": ["Male", "Female"],
+        "Age": [50, 46],
+        "Tenure": [7, 0],
+        "Balance": [0, 0],
+        "NumOfProducts": [2, 1],
+        "HasCrCard": [1, 1],
+        "IsActiveMember": [1, 1],
+        "EstimatedSalary": [10022.8, 6294.84],
+        "Complain": [0, 1],
+        "Satisfaction_Score": [2, 3],
+        "Card_Type": ["SILVER", "GOLD"],
+        "Point_Earned": [205, 345]
+    })
+    labels = predict(classifier, preprocessor, sample)
+    print(labels)
