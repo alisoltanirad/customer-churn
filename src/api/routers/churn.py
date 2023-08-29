c9ce6cbf983cc40a330b6f54f553127096fd171b
alisoltanirad <soltaniradali@gmail.com>
2023-08-29 14:46:33 -0400

    Add Enum variables to the API schema

--- src/api/routers/churn.py
+++ src/api/routers/churn.py
@@ -10,7 +10,7 @@ from fastapi import APIRouter
 
 from api.config import CHURN_URL
 from api.schemas import Request, Response
-from logic.config import GCS_FOLDER
+from logic.config import CATEGORICAL_COLS, GCS_FOLDER
 from logic.gc_utils import load_from_gcs
 from logic.prediction import predict
 
@@ -25,8 +25,15 @@ async def customer_churn(request: Request):
     This route is used to predict if each customer churns.
 
     """
-    # Transform input data for prediction
+    # Transform input data to feed into a pandas dataframe
     customers: list[dict] = [c.dict() for c in request.customers]
+
+    # Unpack enum values for categorical features
+    for customer in customers:
+        for feature in CATEGORICAL_COLS:
+            customer[feature] = customer[feature].value
+
+    # Create pandas dataframe to feed into the preprocessor
     input_data: pd.DataFrame = pd.DataFrame(customers)
 
     # Run prediction pipeline (preprocessing, classification) and get the labels
--- src/api/schemas.py
+++ src/api/schemas.py
@@ -5,7 +5,27 @@ This module contains the I/O models for the API.
 
 """
 
+from enum import Enum
+
 from pydantic import BaseModel, Field
+
+
+class Country(Enum):
+    FRANCE: str = "France"
+    GERMANY: str = "Germany"
+    SPAIN: str = "Spain"
+
+
+class Sex(Enum):
+    MALE: str = "Male"
+    FEMALE: str = "Female"
+
+
+class CardType(Enum):
+    DIAMOND: str = "DIAMOND"
+    GOLD: str = "GOLD"
+    SILVER: str = "SILVER"
+    PLATINUM: str = "PLATINUM"
 
 
 class Customer(BaseModel):
@@ -15,20 +35,20 @@ class Customer(BaseModel):
     predciting if the customer churns.
 
     """
-    CreditScore: int = Field(title="Credit Score")
-    Geography: str = Field(title="Geography (Country)")
-    Gender: str = Field(title="Gender")
-    Age: int = Field(title="Age")
-    Tenure: int = Field(title="Tenure (Household Size)")
-    Balance: float = Field(title="Balance")
-    NumOfProducts: int = Field(title="Number of Products")
-    HasCrCard: int = Field(title="Has Credit Card?")
-    IsActiveMember: int = Field(title="Is Active Member?")
-    EstimatedSalary: float = Field(title="Estimated Salary")
-    Complain: int = Field(title="Has Complain?")
-    Satisfaction_Score: int = Field(title="Satisfaction Score")
-    Card_Type: str = Field(title="Card Type")
-    Point_Earned: int = Field(title="Points Earned")
+    CreditScore: int = Field(..., title="Credit Score")
+    Geography: Country = Field(..., title="Geography (Country)")
+    Gender: Sex = Field(..., title="Gender")
+    Age: int = Field(..., title="Age")
+    Tenure: int = Field(..., title="Tenure (Household Size)")
+    Balance: float = Field(..., title="Balance")
+    NumOfProducts: int = Field(..., title="Number of Products")
+    HasCrCard: int = Field(..., title="Has Credit Card?")
+    IsActiveMember: int = Field(..., title="Is Active Member?")
+    EstimatedSalary: float = Field(..., title="Estimated Salary")
+    Complain: int = Field(..., title="Has Complain?")
+    Satisfaction_Score: int = Field(..., title="Satisfaction Score")
+    Card_Type: CardType = Field(..., title="Card Type")
+    Point_Earned: int = Field(..., title="Points Earned")
 
 
 class Request(BaseModel):
